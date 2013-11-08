523b52bbcf8c0fd40d936fd1494f086f7fdb3c29
Andre Futter <a.futter@bigpoint.net>
2013-11-08 17:20:34 +0100

    ...

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 var socket = io.connect('http://localhost');
+var sessionId = '';
 
 // ================= Generic events  ==================
 
@@ -19,15 +20,23 @@ socket.on('connect', function() {
     log("Connected to Server");
 });
 
+// Server is connected successfully
+socket.on('sessionId', function(data) {
+    log(data);
+    sessionId = data;
+});
 // Connection with server disconnected
 socket.on('disconnect', function() {
     log("Disconnected from Server");
-    log("Hit Ctrl+F5 to start a new game");
+    $('body').css('background-color', '#e74c3c');
+    //log("Hit Ctrl+F5 to start a new game");
 });
 
 // Connection with server was reconnected
 socket.on('reconnect', function() {
     log("Reconnected to Server");
+    
+    window.location = window.location;
 });
 
 // Reconnection with server failed
@@ -42,24 +51,36 @@ socket.on('playerListUpdate', function(data) {
     $list.html('');
     
     var entry = '';
+    var isMe = false;
 
     for (key in data) {
         
-        entry = '<li>' 
-        + data[key].name 
-        + ' (' 
-        + data[key].state
-        + ')';
+        isMe = false;
+        
+        entry = '<li';
+
+        if (data[key].sessionId === sessionId) {
+            isMe = true;
+            entry += ' class="me"';
+        };
+        
+        if (data[key].state === 'playing') {
+            entry += ' class="pulse"';
+        };
+        entry += '>'
+        
+        + '<span class="glyphicon glyphicon-user"></span>' 
+        + '<div class="player-list-name">' + data[key].name + '</div>' 
+        + '<div class="player-list-state">' + data[key].state + '</div>' 
 
         entry += '</li>';
         
         var $entry = $(entry);
-        
-        if (data[key].you) {
-            $(entry).addClass('you');
+        if (isMe) {
+            $list.prepend($entry);
+        } else {
+            $list.append($entry);
         };
-
-        $list.append($entry);
     }
 }); 
 
@@ -70,35 +91,81 @@ socket.on('gameStarted', function(data) {
     $('#playfield').fadeIn();
 }); 
 
+// Game has been started
+socket.on('instruction', function(data) {
+    var $instruction = $('#player-instruction');
+    $instruction.hide();
+    $instruction.text(data);
+    $instruction.fadeIn();
+});
+
+function emitPlayerAction(data) {
+    socket.emit('playerAction', data);
+};
+
+// Game has been started
+socket.on('newDataset', function(data) {
+    
+    console.log(data);
+    
+    $('#dataset-title').text(data.title);
+    $('#dataset-body').html('');
+    
+     var row = '';
+     var prop = {};
+     
+    for (var i in data.props) {
+        prop = data.props[i];
+        row = '<tr '
+        + 'data-id="' + prop.id + '">'
+        + '<td>' + prop.name + '</td>'
+        + '<td>' + prop.value + ' ' + prop.unit + '</td>'
+        row += '</tr>';
+        
+        $('#dataset-body').append(row);
+    };
+
+    $('#dataset-body').data('id', data.id);
+    
+        $('#playfield tr').on('click', function() {
+        emitPlayerAction({
+            propId : $(this).data('id')
+        });
+    });
+}); 
 
 // Displays messages from the server in log (select box)
 function log(txt) {
-  $('#log ul').append('<li>'+ txt + '</li>');
+  $('#log ul').prepend('<li>'+ txt + '</li>');
+  $('#log ul li').first().hide();
+  
+  $('#log ul li').first().fadeIn('fast');
 }
 
 function insertRandomName() {
-  var names = ['Hans', 'Horst', 'Peter', 'Frank', 'Helmut', 'Fritz','Joe', 'John', 'Bill', 'Steve'];
+  var names = ["Aaron", "Adam", "Alan", "Albert", "Alexander", "Alice", "Amanda", "Amber", "Amy", "Andrea", "Andrew", "Angela", "Ann", "Anna", "Anthony", "Arthur", "Ashley", "Austin", "Barbara", "Benjamin", "Betty", "Beverly", "Billy", "Bobby", "Brandon", "Brenda", "Brian", "Brittany", "Bruce", "Bryan", "Carl", "Carol", "Carolyn", "Catherine", "Charles", "Cheryl", "Christian", "Christina", "Christine", "Christopher", "Craig", "Crystal", "Cynthia", "Daniel", "Danielle", "David", "Deborah", "Debra", "Denise", "Dennis", "Diana", "Diane", "Donald", "Donna", "Doris", "Dorothy", "Douglas", "Dylan", "Edward", "Elizabeth", "Emily", "Emma", "Eric", "Ethan", "Eugene", "Evelyn", "Frances", "Frank", "Gary", "George", "Gerald", "Gloria", "Grace", "Gregory", "Hannah", "Harold", "Harry", "Heather", "Helen", "Henry", "Howard", "Jack", "Jacob", "Jacqueline", "James", "Jane", "Janet", "Janice", "Jason", "Jean", "Jeffrey", "Jennifer", "Jeremy", "Jerry", "Jesse", "Jessica", "Joan", "Joe", "John", "Johnny", "Jonathan", "Jordan", "Jose", "Joseph", "Joshua", "Joyce", "Juan", "Judith", "Judy", "Julia", "Julie", "Justin", "Karen", "Katherine", "Kathleen", "Kathryn", "Kathy", "Keith", "Kelly", "Kenneth", "Kevin", "Kimberly", "Kyle", "Larry", "Laura", "Lauren", "Lawrence", "Linda", "Lisa", "Lori", "Louis", "Madison", "Margaret", "Maria", "Marie", "Marilyn", "Mark", "Martha", "Mary", "Matthew", "Megan", "Melissa", "Michael", "Michelle", "Mildred", "Name", "Nancy", "Nathan", "Nicholas", "Nicole", "Number", "Number", "Olivia", "Pamela", "Patricia", "Patrick", "Paul", "Peter", "Philip", "Phillip", "Rachel", "Ralph", "Randy", "Rank", "Raymond", "Rebecca", "Richard", "Robert", "Roger", "Ronald", "Rose", "Roy", "Russell", "Ruth", "Ryan", "Samantha", "Samuel", "Sandra", "Sara", "Sarah", "Scott", "Sean", "Sharon", "Shirley", "Stephanie", "Stephen", "Steven", "Susan", "Tammy", "Teresa", "Terry", "Theresa", "Thomas", "Tiffany", "Timothy", "Tyler", "Victoria", "Vincent", "Virginia", "Walter", "Wayne", "William", "Willie", "Zachary"];
   var name = names[Math.floor(Math.random()*names.length)];
 
-  $('#player-name').val(name + (Math.floor(Math.random()*10000)));
+  $('#player-name').val(name + (Math.floor(Math.random()*10)));
 }
 
 $(document).ready(function() {
     
     insertRandomName();
-    
-    $('.action-button').on('click', function() {
-
-        socket.emit('playerAction', {
-            'id':  $(this).data('id')
-        });
-    });
 
     $('#player-signup').on('submit', function(event) {
         event.preventDefault();
         var frm = $('#player-signup');
         socket.emit('playerJoin', frm.serializeObject());
+        
         $(this).fadeOut();
+        
+        $('#player-header').removeClass('hidden')
+        
+        $('#player-header-name').text(
+            $('#player-name').val()
+        );
+        
     });
 });
 
--- server.js
+++ server.js
@@ -1,8 +1,126 @@
+round = 42;
 
 // Users class
 
+var gameData = [{
+    id: 0,
+    title: 'Car AAAA',
+    props: [{
+       id: 0,
+       name: 'Max. speed',
+       value: 250,
+       unit: 'km/h',
+       comparison: 'high'
+    },{
+       id: 1,
+       name: 'Acceleration',
+       value: 4.5,
+       unit: 'seconds',
+       comparison: 'low'
+   }, {
+       id: 2,
+       name: 'Horsepower',
+       value: 340,
+       unit: 'PS',
+       comparison: 'high'
+   }, {
+       id: 3,
+       name: 'Price',
+       value: 120000,
+       unit: '€',
+       comparison: 'high'
+   }]
+}, {
+    id: 1,
+    title: 'Car BBBB',
+    props: [{
+       id: 0,
+       name: 'Max. speed',
+       value: 275,
+       unit: 'km/h',
+       comparison: 'high'
+    },{
+       id: 1,
+       name: 'Acceleration',
+       value: 5.2,
+       unit: 'seconds',
+       comparison: 'low'
+   }, {
+       id: 2,
+       name: 'Horsepower',
+       value: 180,
+       unit: 'PS',
+       comparison: 'high'
+   }, {
+       id: 3,
+       name: 'Price',
+       value: 200000,
+       unit: '€',
+       comparison: 'high'
+   }]
+}];
+
+function getRandomDataset() {
+    var r = Math.floor(Math.random() * gameData.length);
+    return gameData[r];
+}
+
+function getDataset(id) {
+    return gameData[id];
+}
+
+var User = function(options) {
+    
+    var name        = options.name;
+    var id          = options.id;
+    var sessionId   = options.sessionId;
+    var socket      = options.socket;
+    var state       = options.state;
+    var dataset     = -1;
+    
+    // get username of user
+    this.getUsername = function() {
+        return name;
+    };
+    
+    // get socket.io client ID
+    this.getId = function() {
+        return id;
+    };
+    
+    this.getSessionId = function() {
+        return sessionId;
+    };
+
+    this.getSocket = function() {
+        return socket;
+    };
+
+    this.getState = function() {
+        return state;
+    };
+    
+    this.setState = function(newState) {
+        state = newState;
+    };
+    
+    this.getDataset = function() {
+        return dataset;
+    };
+    
+    this.setDataset = function(datasetId) {
+        dataset = datasetId;
+    };
+}
+
 var UserStorage = function () {
     var users = [];
+    
+    // remove specific user from storage
+    this.removeUser = function(userId) {
+        var user = this.getUserById(userId);
+        users.splice(users.indexOf(user), 1);
+    };
     
     // add new user
     this.addUser = function(userObject) {
@@ -12,43 +130,13 @@ var UserStorage = function () {
     // get user by id
     this.getUserById = function(userId) {
         for (var i = 0; i < users.length; i++) {
-            if (users[i].id == userId) {
+            if (users[i].getId() == userId) {
                 return users[i];
             }
         };
         return null;
     }
-    
-    // remove specific user from storage
-    this.removeUser = function(userId) {
-        var user = this.getUserById(userId);
-        users.splice(users.indexOf(user), 1);
-    };
-    
-    // get username of specific user from storage
-    this.getUserName = function(userId) {
-        var user = this.getUserById(userId);
-        return user.name;
-    };
-    
-    // assign name to specific user
-    this.setUserName = function(userId, name) {
-        var user = this.getUserById(userId);
-        user.name = name;
-    };
-    
-    // increase number of attempts
-    this.increaseAttempts = function(userId) {
-        var user = this.getUserById(userId);
-        user.attempts++;
-    };
-    
-    // return number of attempts
-    this.getAttempts = function(userId) {
-        var user = this.getUserById(userId);
-        return user.attempts;
-    };
-    
+
     // print info about all users
     this.printUsers = function() {
     
@@ -56,7 +144,7 @@ var UserStorage = function () {
         console.log('ID\t\t\tName');
     
         for (var i = 0; i < users.length; i++) {
-            console.log(users[i].id + '\t' + users[i].name + '\t');
+            console.log(users[i].getId() + '\t' + users[i].getUsername() + '\t');
         }
         console.log('--------------------------------');
     };
@@ -76,15 +164,81 @@ var UserStorage = function () {
         var list = [];
         for (var i = 0; i < users.length; i++) {
             list.push({
-                name : users[i].name,
-                state: users[i].state,
-                you:   users[i].id === client.id
+                name : users[i].getUsername(),
+                state: users[i].getState(),
+                sessionId: users[i].getSessionId()
             });
         }
         return list;
     }
 
 };
+
+/************************************************************************************/
+
+ var Round = function() {
+        
+        var activePlayer = {};
+        var waitingPlayer = {};
+        var players = [];
+        var active = false;
+        
+        this.addPlayers = function(playersArray) {
+            players = playersArray;
+            
+            //default
+            activePlayer = players[0];
+            waitingPlayer = players[1];
+            
+        };
+        
+        this.getActivePlayer = function() {
+            return activePlayer;
+        };
+        
+        this.getWaitingPlayer = function() {
+            return waitingPlayer;
+        };
+        
+        this.swapPlayers = function() {
+            var temp = waitingPlayer;
+            waitingPlayer = activePlayer;
+            activePlayer = temp;
+        };
+         
+        this.isActive = function() {
+            return active;
+        };               
+
+        // start new round
+        this.start = function() {
+            
+            activePlayerSocket = activePlayer.getSocket();
+            waitingPlayerSocket = waitingPlayer.getSocket();
+            
+            activePlayer.setState('playing');
+            activePlayerSocket.emit('news', "It's your turn, " + activePlayer.getUsername() + '!');
+            activePlayerSocket.emit('instruction', "Choose your most competitive spec!");
+            
+            waitingPlayer.setState('waiting');
+            waitingPlayerSocket.emit('news', "Please wait, " + waitingPlayer.getUsername() + '!');
+            waitingPlayerSocket.emit('instruction', "Please wait for " + activePlayer.getUsername() + '!');
+            
+            io.sockets.emit('playerListUpdate', userStorage.getPlayerList());
+
+            //var dataset = getRandomDataset();
+            var dataset = gameData[0];
+            activePlayerSocket.emit('newDataset', dataset);
+            activePlayer.setDataset(dataset.id);
+            
+            //dataset = getRandomDataset();
+            dataset = gameData[1];
+            waitingPlayerSocket.emit('newDataset', dataset);
+            waitingPlayer.setDataset(dataset.id);
+            
+            active = true;
+        };
+    };
 
 /************************************************************************************/
 
@@ -108,16 +262,18 @@ app.get('/style.css', function (req, res) {
 
 
 var userStorage = new UserStorage();
+var round = new Round();
+
 
 io.sockets.on('connection', function(client) {
-    
+
   client.on('disconnect', function() {
       if (userStorage.getUserById(client.id) !== null) {
-        var username = userStorage.getUserName(client.id);
+        var username = userStorage.getUserById(client.id).getUsername();
         userStorage.removeUser(client.id);
         
         io.sockets.emit('news', username + ' has left the game.');
-        io.sockets.emit('playerListUpdate', userStorage.getPlayerList(client));
+        io.sockets.emit('playerListUpdate', userStorage.getPlayerList());
       };
   });
 
@@ -125,8 +281,9 @@ io.sockets.on('connection', function(client) {
         'userCount': userStorage.count()
     });
     
-    client.emit('playerListUpdate', userStorage.getPlayerList(client));
-
+    client.emit('playerListUpdate', 
+        userStorage.getPlayerList()
+    );
 
     client.on('playerJoin', function(data) {
         
@@ -134,67 +291,81 @@ io.sockets.on('connection', function(client) {
         
         if (userStorage.count() < MAX_PLAYERS) {
 
-            var user = {
+            var user = new User({
                 name : data.username,
                 id : client.id,
+                sessionId : Math.random().toString(36).slice(2),
                 socket: client,
-                state: 'idle'
-            };
-            
+                state: 'idle',
+                dataset: -1
+            });
+
             userStorage.addUser(user);
     
             io.sockets.emit('news', data.username + ' has joined. Waiting for ' + Number(MAX_PLAYERS - userStorage.count())  + ' player(s)');
-            io.sockets.emit('playerListUpdate', userStorage.getPlayerList(client));
+            client.emit('sessionId', user.sessionId);
+            io.sockets.emit('playerListUpdate', userStorage.getPlayerList());
             
             if (userStorage.count() === MAX_PLAYERS) {
-                startGame();
+                startGame(round);
             };
             
         } else {
             io.sockets.emit('news', 'Sorry, ' + data.username + '! We are full.');
         };
-    }); 
+    });
     
-    function startGame() {
-        var players = userStorage.getPlayers();
+    client.on('playerAction', function(data) {
         
-        var round = new Round(players);
+        if (true === round.isActive()) {
+
+            var username = userStorage.getUserById(client.id).getUsername();
+            
+            var activePlayer = round.getActivePlayer();
+            var waitingPlayer = round.getWaitingPlayer();
+            
+            if (client.id === activePlayer.getId()) {
+                
+                var apDataset = getDataset(activePlayer.getDataset());
+                var wpDataset = getDataset(waitingPlayer.getDataset());
+                
+                var apProp = apDataset.props[data.propId];
+                var wpProp = wpDataset.props[data.propId];
+                
+                
+                console.log(apProp.value + ' vs. ' + wpProp.value);
+                
+                var apIsWinner = false;
+                
+                if(apProp.comparison === 'low') {
+                    apIsWinner = (apProp.value < wpProp.value);
+                } else if (apProp.comparison === 'high')
+                    apIsWinner = (apProp.value > wpProp.value);
+                };
+                
+                if (apIsWinner) {
+                    io.sockets.emit('news', activePlayer.getUsername() + ' wins!');
+                } else {
+                    io.sockets.emit('news', waitingPlayer.getUsername() + ' loses!');
+                    round.swapPlayers();
+                };
+
+                round.start();
+                
+            } else {
+                client.emit('news', 'Sorry, ' + username + '! It is not your turn');
+            }
+    });
+    
+    function startGame(round) {
+        
+        round.addPlayers(userStorage.getPlayers());
         round.start();
         
         io.sockets.emit('gameStarted');
     }
     
-    var Round = function(players) {
-        
-        var activePlayer = {};
-        var waitingPlayer = {};
-        
-        client.on('playerAction', function(data) {
-            
-            var username = userStorage.getUserName(client.id);
-            
-            if (client.id === activePlayer.id) {
-                io.sockets.emit('news', username + ' pressed button #' + data.id);
-            } else {
-                io.sockets.emit('news', 'Sorry, ' + username + '! It is not your turn');
-            }
-        });
-
-        // start new round
-        this.start = function() {
-            activePlayer = players[0];
-            waitingPlayer = players[1];
-            
-            activePlayer.state = 'playing';
-            activePlayer.socket.emit('news', "It's your turn, " + activePlayer.name + '!');
-            
-            waitingPlayer.state = 'waiting';
-            waitingPlayer.socket.emit('news', "Please wait, " + waitingPlayer.name + '!');
-            
-            client.emit('playerListUpdate', userStorage.getPlayerList(client));
-        }
-    };
-    
+   
 });
 
 
